58ab090bc9c13d5b284d65235ff30a8e74b1acfe
Ammar Khan <ammarkofficial@gmail.com>
2023-03-23 14:01:51 +0500

    Day-1 Python Veriables Completed

---
+++ Day-1/Veriable.py
@@ -0,0 +1,24 @@
+students_count = 1000
+print(students_count)
+# Integer type veriable
+rating = 4.99
+print(rating)
+# Decimal type veriable
+is_published = False
+print(is_published)
+# Boolean type veriabel
+course_name="Pyhton Veriables"
+print(course_name)
+# String type veriable
+
+
+
+print("Some good practice rules")
+
+# Verialble name is lable where the Value is storted
+# Python is case sensitive
+# Veriable name must be discriptive and meaningfull
+# Using lower case veriable names is good practice
+# We cannot have space in names so _ is good pratice
+# Try to add spaces around special craracters like = + is good practice
+# Symbol is used for commenting
